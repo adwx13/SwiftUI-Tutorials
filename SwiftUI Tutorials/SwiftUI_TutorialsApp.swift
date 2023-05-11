1eadd8142cc82ec61c022e6efff5d6cda48aa9af
sungjin <sungjinlee.kor@gmail.com>
2023-05-12 07:49:26 +0900

    Initial Commit

---
+++ SwiftUI Tutorials/SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialsApp.swift
+//  SwiftUI Tutorials
+//
+//  Created by 이성진 on 2023/05/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
